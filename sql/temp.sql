dcb8b0900f712bd47028b32a97dd404ec292df3b
Bekzhan Satarov <satarovbekzhan@gmail.com>
2021-06-24 01:01:07 +0200

    Backup data

--- sql/temp.sql
+++
@@ -1,9 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [id]
-                ,[product]
-                ,[value]
-                ,[starting]
-FROM [mmm].[dbo].[price]
-WHERE starting IN (SELECT max(starting) FROM [mmm].[dbo].[price]);
-
-DELETE FROM [mmm].[dbo].[price]
